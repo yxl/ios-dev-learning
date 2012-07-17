33b3f1f69526c164c61af71f57eb5f5b09a36bab
Yuan Xulei <xyuan@mozilla.com>
2012-07-17 18:24:40 +0800

    add DynamicViews project.

---
+++ DynamicViews/DynamicViews/DynamicViewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DynamicViewsViewController.h
+//  DynamicViews
+//
+//  Created by Online Mozilla on 7/17/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DynamicViewsViewController : UIViewController
+
+@end
